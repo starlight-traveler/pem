d04be3e6c588e1281228967e981b4be2dc279b0d
RND <123332740+RP-ND@users.noreply.github.com>
2023-12-01 14:00:23 -0500

    Update pem.ino

--- PEM/pem.ino
+++ PEM/pem.ino
@@ -12,77 +12,97 @@ __/\\\\\\\\\\\\\____/\\\\\\\\\\\\\\\__/\\\\____________/\\\\_
 
 #include "config.h"
 
+Adafruit_MPL3115A2 altimeter = Adafruit_MPL3115A2();
+
 void setup() {
-    Serial.begin(115200);
-    while (!Serial) {
-        delay(1);
-    }
+  Serial.begin(115200);
 
-    initializeRadio();
+  initializeRadio();
 
-    // Set pin modes for new pins
-    pinMode(CHECK_PIN, INPUT);
-    pinMode(WRITE_PIN, OUTPUT);
+  // Set pin modes for new pins
+  pinMode(CHECK_PIN, INPUT);
+  pinMode(WRITE_PIN, OUTPUT);
+  pinMode(ALTITUDE_READY_PIN, OUTPUT);  // Pin to indicate altitude condition met
 
-    // Initialize NeoPixel
-    pixels.begin();
-    pixels.show(); // Initialize all pixels to 'off'
 
+  // Initialize NeoPixel
+  pixels.begin();
+  pixels.show();  // Initialize all pixels to 'off'
+
+  // Initialize MPL3115A2
+  if (!altimeter.begin()) {
+    Serial.println("Could not find a valid MPL3115A2 sensor, check wiring!");
+    while (1)
+      ;
+  }
 }
 
+
 void loop() {
-    String message;
-    bool received = false;
-    bool continueChecking = false;
+  String message;
+  bool received = false;
+  bool continueChecking = false;
 
-    // Continuously check for received messages
-    while (!received) {
-        // Set the NeoPixel to yellow, indicating waiting for message
-        setNeoPixelColor(pixels.Color(255, 255, 0));
+  // Continuously check for received messages
+  while (!received) {
+    // Set the NeoPixel to yellow, indicating waiting for message
+    setNeoPixelColor(pixels.Color(255, 255, 0));
 
-        received = checkForReceivedMessage(message);
-        if (received) {
-            // If TARGET_STRING is received, activate continuous checking
-            if (message == TARGET_STRING) {
-                continueChecking = true; // Enable continuous checking of CHECK_PIN
-                // Set the NeoPixel to green, indicating message received
-                setNeoPixelColor(pixels.Color(0, 255, 0));
-                Serial.println("Target string received, starting continuous pin check.");
-                break;
-            }
-        }
+    received = checkForReceivedMessage(message);
+    if (received) {
+      // If TARGET_STRING is received, activate continuous checking
+      if (message == TARGET_STRING) {
+        continueChecking = true;  // Enable continuous checking of CHECK_PIN
+        // Set the NeoPixel to green, indicating message received
+        setNeoPixelColor(pixels.Color(0, 255, 0));
+        Serial.println("Target string received, starting continuous pin check.");
+        break;
+      } else {
+        // If received string is not the TARGET_STRING, continue listening
+        received = false;  // Reset the received flag
+        message = "";      // Clear the message buffer
+      }
     }
+  }
 
-    // Once a message is received, enter a new loop to continuously check the CHECK_PIN
-    while (continueChecking) {
-        if (digitalRead(CHECK_PIN) == HIGH) {
-            
-            // Set the WRITE_PIN to HIGH
-            digitalWrite(WRITE_PIN, HIGH);
-            // Set the NeoPixel to red, indicating CHECK_PIN is high
-            setNeoPixelColor(pixels.Color(255, 0, 0));
-            // Leave once pin written to hight
-            break;
-        } else {
-            // Set the NeoPixel to purple, indicating CHECK_PIN is not high
-            setNeoPixelColor(pixels.Color(128, 0, 128));
-        }
+  // Once a message is received, enter a new loop to continuously check the CHECK_PIN
+  while (continueChecking) {
+    if (digitalRead(CHECK_PIN) == HIGH) {
+
+      // Set the WRITE_PIN to HIGH
+      digitalWrite(WRITE_PIN, HIGH);
+      // Set the NeoPixel to red, indicating CHECK_PIN is high
+      setNeoPixelColor(pixels.Color(255, 0, 0));
+      // Leave once pin written to hight
+      break;
+    } else {
+      // Set the NeoPixel to purple, indicating CHECK_PIN is not high
+      setNeoPixelColor(pixels.Color(128, 0, 128));
     }
-    
-    // Transmit that system has fired
-    while (1) {
+  }
 
-      transmitMessage(TRANSMIT_STRING);
-
+  while (1) {
+    bool altitudeReached = checkAndSetAltitude();
+    if (altitudeReached) {
+      Serial.println("Target altitude reached, exiting loop.");
+      break;  // Break out of the loop once target altitude is reached
     }
+  }
 
+  setNeoPixelColor(pixels.Color(0, 0, 255));  // Change NeoPixel color to blue
+
+  // Transmit that system has fired
+  while (1) {
+
+    transmitMessage(TRANSMIT_STRING);
+  }
 }
 
 void setNeoPixelColor(uint32_t color) {
-    for (int i = 0; i < NUMPIXELS; i++) {
-        pixels.setPixelColor(i, color);
-    }
-    pixels.show();
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, color);
+  }
+  pixels.show();
 }
 
 /*

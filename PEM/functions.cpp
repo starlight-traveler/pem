b75febd83669d74a556aadebe90c3a9cc47a34ec
RND <123332740+RP-ND@users.noreply.github.com>
2023-12-01 14:00:39 -0500

    Update functions.cpp

--- PEM/functions.cpp
+++ PEM/functions.cpp
@@ -5,64 +5,90 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + 
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 void initializeRadio() {
-    pinMode(RFM95_RST, OUTPUT);
-    digitalWrite(RFM95_RST, HIGH);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
 
-    // Manual reset
-    digitalWrite(RFM95_RST, LOW);
-    delay(10);
-    digitalWrite(RFM95_RST, HIGH);
-    delay(10);
+  // Manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
 
-    while (!rf95.init()) {
-        Serial.println("LoRa failed to initialize!");
-        while (1);
-    }
-    Serial.println("LoRa radio init OK!");
+  while (!rf95.init()) {
+    Serial.println("LoRa failed to initialize!");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
 
-    if (!rf95.setFrequency(RF95_FREQ)) {
-        Serial.println("setFrequency failed!");
-        while (1);
-    }
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed!");
+    while (1);
+  }
 
-    // Configure LoRa transmitter settings
-    rf95.setTxPower(TX_POWER, false);
-    rf95.setSignalBandwidth(BANDWIDTH * 1000); // Convert kHz to Hz
-    rf95.setSpreadingFactor(SPREADING_FACTOR);
-    rf95.setCodingRate4(CODING_RATE);
-    rf95.setPreambleLength(PREAMBLE_LENGTH);
+  // Configure LoRa transmitter settings
+  rf95.setTxPower(TX_POWER, false);
+  rf95.setSignalBandwidth(BANDWIDTH * 1000);  // Convert kHz to Hz
+  rf95.setSpreadingFactor(SPREADING_FACTOR);
+  rf95.setCodingRate4(CODING_RATE);
+  rf95.setPreambleLength(PREAMBLE_LENGTH);
 }
 
 bool checkForReceivedMessage(String &message) {
-    if (rf95.available()) {
-        uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-        uint8_t len = sizeof(buf);
+  if (rf95.available()) {
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
 
-        if (rf95.recv(buf, &len)) {
-            digitalWrite(LED_BUILTIN, HIGH);
-            message = String((char*)buf);
-            Serial.print("Got: "); Serial.println(message);
-            digitalWrite(LED_BUILTIN, LOW);
-            return true;
-        } else {
-            Serial.println("Receive Failed.");
-        }
+    if (rf95.recv(buf, &len)) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      message = String((char *)buf);
+      Serial.print("Got: ");
+      Serial.println(message);
+      digitalWrite(LED_BUILTIN, LOW);
+      return true;
+    } else {
+      Serial.println("Receive Failed.");
     }
-    return false;
+  }
+  return false;
+}
+
+bool altitudeTargetSet = false;
+float triggerAltitude = 0.0;
+
+bool checkAndSetAltitude() {
+  float altitude = altimeter.getAltitude();  // Read current altitude
+  Serial.print("Current Altitude: ");
+  Serial.println(altitude);
+
+  if (!altitudeTargetSet) {
+    triggerAltitude = altitude + 200.0;  // Add 200 feet to the initial altitude
+    altitudeTargetSet = true;            // Ensure this block runs only once
+    Serial.print("Target Altitude Set: ");
+    Serial.println(triggerAltitude);
+  }
+
+  if (altitude >= triggerAltitude) {
+    digitalWrite(ALTITUDE_READY_PIN, HIGH);  // Set the pin high
+    Serial.println("Altitude condition met, setting pin high.");
+    return true;  // Return true when target altitude is reached
+  }
+
+  return false;  // Return false otherwise
 }
 
 void transmitMessage(const String &message) {
-    Serial.print("Transmitting: "); Serial.println(message);
-    
-    // Convert the message to a byte array for transmission
-    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-    message.getBytes(buf, RH_RF95_MAX_MESSAGE_LEN);
-    size_t len = message.length();
+  Serial.print("Transmitting: ");
+  Serial.println(message);
 
-    // Send the message
-    if (rf95.send(buf, len)) {
-        Serial.println("Message sent successfully");
-    } else {
-        Serial.println("Message failed to send");
-    }
+  // Convert the message to a byte array for transmission
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  message.getBytes(buf, RH_RF95_MAX_MESSAGE_LEN);
+  size_t len = message.length();
+
+  // Send the message
+  if (rf95.send(buf, len)) {
+    Serial.println("Message sent successfully");
+  } else {
+    Serial.println("Message failed to send");
+  }
 }
